77cc71389e56ff985788f0a1b92a50c7a268cb2c
Samarth Mabrukar <samarth.mabrukar@gmail.com>
2018-09-22 17:36:01 +0530

    Secen2 Basic GLM

---
+++ Scene2/Source.cpp
@@ -0,0 +1,352 @@
+#include"CommonHeader_Scene2.h"
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+FILE *gpFile;
+HWND ghwnd;
+HDC ghdc;
+HGLRC ghrc;
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbActiveWindow = false;
+bool gbFullscreen = false;
+bool gbIsEscapeKeyPressed = false;
+bool gbAnimate = false;
+bool gbLight = false;
+bool gbIsAKeyPressed = false;
+bool gbIsLKeyPressed = false;
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	void initialize(void);
+	void display(void);
+	void update(void);
+	void uninitialize(int);
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szClassName[] = TEXT("My App");
+	bool bDone = false;
+
+	if (fopen_s(&gpFile, "Log.txt", "w") != NULL)
+	{
+		MessageBox(NULL, TEXT("Cannot Create Log File !!!"), TEXT("Error"), MB_OK);
+		exit(EXIT_FAILURE);
+	}
+	else
+		fprintf(gpFile, "Log File Created Successfully...\n");
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.lpszClassName = szClassName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW, szClassName, TEXT("Engine Working"), WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, WIN_WIDTH, WIN_HEIGHT, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL)
+	{
+		fprintf(gpFile, "Cannot Create Window...\n");
+		uninitialize(1);
+	}
+
+	ghwnd = hwnd;
+
+	ShowWindow(hwnd, iCmdShow);
+	SetFocus(hwnd);
+	SetForegroundWindow(hwnd);
+
+	initialize();
+
+	// MessageBox(hwnd, TEXT("After init"), TEXT("MSG"), MB_OK);
+
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				if (gbIsEscapeKeyPressed == true)
+					bDone = true;
+				
+				if (gbAnimate == true)
+					update();
+				
+				display();
+			}
+		}
+	}
+
+	uninitialize(0);
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	void resize(int, int);
+	void ToggleFullscreen(void);
+	switch (iMsg)
+	{
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0)
+			gbActiveWindow = true;
+		else
+			gbActiveWindow = false;
+		break;
+	case WM_CREATE:
+		break;
+	case WM_SIZE:
+		currentWidth = LOWORD(lParam);
+		currentHeight = HIWORD(lParam);
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			gbIsEscapeKeyPressed = true;
+			break;
+		case 0x46:
+			if (gbFullscreen == false)
+			{
+				ToggleFullscreen();
+				gbFullscreen = true;
+			}
+			else
+			{
+				ToggleFullscreen();
+				gbFullscreen = false;
+			}
+			break;
+
+		case 0x41:
+			if (gbIsAKeyPressed == false)
+			{
+				gbAnimate = true;
+				gbIsAKeyPressed = true;
+			}
+			else
+			{
+				gbAnimate = false;
+				gbIsAKeyPressed = false;
+			}
+			break;
+
+		case 0x4C:
+			if (gbIsLKeyPressed == false)
+			{
+				gbLight = true;
+				gbIsLKeyPressed = true;
+			}
+			else
+			{
+				gbLight = false;
+				gbIsLKeyPressed = false;
+			}
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void initialize(void)
+{
+	void Scene2_Initialize(void);
+
+	void resize(int, int);
+	void uninitialize(int);
+	void CreatePoints_Inlet(GLint iWidth, GLint iHeight);
+	void CreatePoints_Outlet(GLint iWidth, GLint iHeight);
+	void CreatePoints_ParticleSpark(GLint iWidth, GLint iHeight);
+
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 24;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+
+	ghdc = GetDC(ghwnd);
+	if (ghdc == NULL)
+	{
+		fprintf(gpFile, "GetDC() Failed.\n");
+		uninitialize(1);
+	}
+
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		fprintf(gpFile, "ChoosePixelFormat() Failed.\n");
+		uninitialize(1);
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		fprintf(gpFile, "SetPixelFormat() Failed.\n");
+		uninitialize(1);
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL)
+	{
+		fprintf(gpFile, "wglCreateContext() Failed.\n");
+		uninitialize(1);
+	}
+
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFile, "wglMakeCurrent() Failed");
+		uninitialize(1);
+	}
+
+	GLenum glew_error = glewInit();
+	if (glew_error != GLEW_OK)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	// Call Scene 2 Function Here
+	Scene2_Initialize();
+
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+
+	glClearColor(0.75f, 0.75f, 0.75f, 0.0f);
+
+	g_scene2_1_PerspectiveProjectionMatrix = glm::mat4(1.0f);
+
+	resize(WIN_WIDTH, WIN_HEIGHT);
+}
+
+
+void display(void)
+{
+	void Scene2_Display(void);
+
+	Scene2_Display();
+
+	SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+	void Scene2_Update(void);
+	Scene2_Update();
+}
+
+
+
+/*void resize(int width, int height)
+{
+	if (height == 0)
+		height = 1;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	g_scene2_1_PerspectiveProjectionMatrix = perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}*/
+
+void ToggleFullscreen(void)
+{
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+	}
+
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+}
+
+void uninitialize(int i_Exit_Flag)
+{
+	void Scene2_UnInitialize(int i_Exit_Flag);
+	if (gbFullscreen == false)
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+
+	Scene2_UnInitialize(i_Exit_Flag);
+
+	wglMakeCurrent(NULL, NULL);
+
+	if (ghrc != NULL)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	if (ghdc != NULL)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (i_Exit_Flag == 0)
+		fprintf(gpFile, "Log File Closed Successfully");
+	else if (i_Exit_Flag == 1)
+		fprintf(gpFile, "Log File Closed Erroniously");
+
+	fclose(gpFile);
+	gpFile = NULL;
+
+	DestroyWindow(ghwnd);
+}
+
